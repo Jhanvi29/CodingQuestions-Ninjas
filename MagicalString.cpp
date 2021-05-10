cc4aef0aed138fba166d81fbccc78a3b7569077c
Jhanvi29 <65902206+Jhanvi29@users.noreply.github.com>
2021-05-10 21:14:56 +0530

    Add files via upload

---
+++ MagicalString.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+string magicalString(string &s){
+	// Write your code here.
+    stack<char> st;
+    int i=0;
+    while(i < s.length()){
+        if(!st.empty() && abs(s[i] - st.top()) == 32){
+            st.pop();
+            i++;
+        }
+        else {
+            st.push(s[i]);
+            i++;
+        }
+        
+    }
+        string short_string = ""; 
+        while (!st.empty())
+        {
+            short_string = st.top() + 
+                           short_string;
+            st.pop();
+        }
+        return (short_string);
+    
+    
+}
